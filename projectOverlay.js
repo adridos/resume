ce685ee90cc2251b7be58ef9633979094d3f3bd7
Adri Massey <adriana.dostine@hotmail.com>
2023-04-26 17:35:34 -0400

    added overlay to projects

---
+++ projectOverlay.js
@@ -0,0 +1,16 @@
+const workBoxes = document.querySelectorAll('.work-box');
+workBoxes.forEach(workBox => {
+  workBox.addEventListener('click', () => {
+    const id = workBox.getAttribute('id');
+    const overlay = document.querySelector(`#${id}-overlay`);
+    overlay.style.display = 'block';
+  });
+});
+
+const closeButtons = document.querySelectorAll('.close-button');
+closeButtons.forEach(closeButton => {
+  closeButton.addEventListener('click', () => {
+    const overlay = document.querySelector(`.overlay`);
+    overlay.style.display = 'none';
+  });
+});
