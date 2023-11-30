e76bb8a4a8055a71031057bd238b78178e0b54d1
Adri Dostine <66699146+adridos@users.noreply.github.com>
2023-11-29 22:55:46 -0500

    code clean up

--- assets/js/autoscroll.js
+++ assets/js/autoscroll.js
@@ -1,27 +1,26 @@
- // Get references to the sections
- const section1 = document.getElementById('welcomePage');
- const section2 = document.getElementById('header');
+// Get references to the sections
+const section1 = document.getElementById("welcomePage");
+const section2 = document.getElementById("header");
 
- // Function to hide section1 and show section2 after a delay
- function autoScroll() {
-   //section1.style.display = 'none';
-   section2.style.display = 'flex';
+// Function to hide section1 and show section2 after a delay
+function autoScroll() {
+  //section1.style.display = 'none';
+  section2.style.display = "flex";
 
-   // Scroll to the top of section2
-   window.scrollTo({
-     top: section2.offsetTop,
-     behavior: 'smooth',
-   });
- }
+  // Scroll to the top of section2
+  window.scrollTo({
+    top: section2.offsetTop,
+    behavior: "smooth",
+  });
+}
 
- setTimeout(autoScroll, 3000); // 3000 milliseconds = 3 seconds
+setTimeout(autoScroll, 3000); // 3000 milliseconds = 3 seconds
 
-
- //this is for the refresh to start over
- window.onload = function () {
-    // Scroll to the top of section1
-    window.scrollTo({
-      top: section1.offsetTop,
-      behavior: 'smooth'
-    });
-  };
+//this is for the refresh to start over
+window.onload = function () {
+  // Scroll to the top of section1
+  window.scrollTo({
+    top: section1.offsetTop,
+    behavior: "smooth",
+  });
+};
