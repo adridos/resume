4d87faf66fa3ad2e14d51dc4f36344c12a1e5d27
Adri Dostine <66699146+adridos@users.noreply.github.com>
2023-11-29 22:50:36 -0500

    finalized styling

--- assets/js/autoscroll.js
+++ assets/js/autoscroll.js
@@ -1,21 +1,23 @@
  // Get references to the sections
  const section1 = document.getElementById('welcomePage');
- const section2 = document.getElementById('theRest');
+ const section2 = document.getElementById('header');
 
  // Function to hide section1 and show section2 after a delay
  function autoScroll() {
-   section1.style.display = 'none';
+   //section1.style.display = 'none';
    section2.style.display = 'flex';
 
    // Scroll to the top of section2
    window.scrollTo({
      top: section2.offsetTop,
-     behavior: 'smooth'
+     behavior: 'smooth',
    });
  }
 
- setTimeout(autoScroll, 3000); // 3000 milliseconds = 5 seconds
+ setTimeout(autoScroll, 3000); // 3000 milliseconds = 3 seconds
 
+
+ //this is for the refresh to start over
  window.onload = function () {
     // Scroll to the top of section1
     window.scrollTo({
